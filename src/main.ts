344fb74e7d1c13ab45fe941df35cc592b2604bfe
Jon-Rabideau <64810994+Jon-Rabideau@users.noreply.github.com>
2021-04-06 19:59:54 -0400

    added firebase stuff
    ^^

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCamMjuzcTZS_rDhs3XC88qTWGXlCbeWL0",
+  authDomain: "restaurant-res-26e48.firebaseapp.com",
+  projectId: "restaurant-res-26e48",
+  storageBucket: "restaurant-res-26e48.appspot.com",
+  messagingSenderId: "320838581873",
+  appId: "1:320838581873:web:85c9586e0a749c13a9a5d0",
+  measurementId: "G-23Q5BE0VF8",
+};
+firebase.initializeApp(firebaseConfig);
+Vue.prototype.$appDB = firebase.firestore();
+Vue.prototype.$appAuth = firebase.auth();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
