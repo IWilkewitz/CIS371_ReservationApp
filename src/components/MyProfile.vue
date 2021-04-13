509301e34d398d712dff784994204a155df1198c
erinelong <longer@mail.gvsu.edu>
2021-04-13 15:03:50 -0400

    Update MyProfile.vue
    Added new bootstrap card for profile section

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -2,22 +2,29 @@
     <div id="MyProfile">
         <h1>My Profile</h1>
         <div id="my-reservations">
-            <h3>My Reservations</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date:</th>
-                        <th>Time:</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(z,pos) in reservations" :key="pos">
-                        <td>{{z.date}}</td>
-                        <td>${{z.time}}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <button type="button" id="cancelReservation" v-on:click="cancelReservation()">Cancel Reservation</button>
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">My Reservations</h5>
+                <h6 class="card-subtitle mb-2 text-muted">View or Change Your Current Reservation(s)</h6>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date:</th>
+                            <th>Time:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(z,pos) in reservations" :key="pos">
+                            <td>{{z.date}}</td>
+                            <td>${{z.time}}</td>
+                        </tr>
+                    </tbody>
+                    <br/>
+                <button type="button" id="cancelReservation" v-on:click="cancelReservation()">Cancel Reservation</button>
+                </table>
+            </div>
+        </div>
+            
         </div>
         <div id="account-details">
             <h3>My Account Details</h3>
@@ -32,7 +39,10 @@
             <label>Need to Change Your Password?</label>
             <br/>
             <button type="button" id="changePassword" v-on:click="changePassword()">Change Password</button>
+            
         </div>
+
+        
     </div>
 </template>
 
