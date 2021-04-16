41d681bd41fea8e77ef985cd2ea46b733345fb7a
Jon-Rabideau <64810994+Jon-Rabideau@users.noreply.github.com>
2021-04-16 14:53:40 -0400

    Restaurants now show up on home page

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,51 @@
 <template>
   <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">Restaurant Reservation Application</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/"
+              >Home <span class="sr-only">(current)</span></a
+            >
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Restaurants</a>
+            <a class="nav-link" href="MyRestaurant">My Restaurants</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">My Profile</a>
+            <a class="nav-link" href="MyProfile">My Profile</a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search for a Restaurant" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search for a Restaurant"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
         </form>
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-             <a class="nav-link" href="#">Sign In <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#"
+              >Sign In <span class="sr-only">(current)</span></a
+            >
           </li>
         </ul>
-            
       </div>
     </nav>
     <div id="components">
@@ -38,66 +56,82 @@
     <div id="logout">
       <button v-if="userLoggedIn()">Logout</button>
     </div>
+   
   </div>
-
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
 import MyRestaurant from "./components/MyRestaurant.vue";
-import { FirebaseFirestore } from "@firebase/firestore-types";
-import { FirebaseAuth } from "@firebase/auth-types";
-
-@Component({
-  components: {
-    MyRestaurant
-import MyProfile from './components/MyProfile.vue';
-import Login from './components/Login.vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Home from "./components/Home.vue";
+import {
+  FirebaseFirestore,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+} from "@firebase/firestore-types";
+import MyProfile from "./components/MyProfile.vue";
+import Login from "./components/Login.vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 import { FirebaseAuth, UserCredential } from "@firebase/auth-types";
-
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 @Component({
   components: {
-    HelloWorld, MyProfile, Login
+    MyProfile,
+    Login,
+    MyRestaurant,
+    Home
   },
 })
-
 export default class App extends Vue {
   readonly $appDB!: FirebaseFirestore;
   readonly $appAuth!: FirebaseAuth;
   readonly $router!: any;
+  private restaurants: any[] = [];
   mounted(): void {
-    console.log("Test")
+    console.log("Test");
+    this.$appDB
+      .collection("restaurants")
+      .orderBy("name")
+      .onSnapshot((qs: QuerySnapshot) => {
+        this.restaurants.splice(0);
+        qs.forEach((qds: QueryDocumentSnapshot) => {
+          if (qds.exists) {
+            const restData = qds.data();
+            this.restaurants.push({
+              name: restData.name,
+              cuisine: restData.cuisine,
+              address: restData.address,
+              phone: restData.phone,
+              picture: restData.picture,
+            });
+          }
+        });
+      });
+    
   }
 
-  GoToAddRestaurant(): void{
-    this.$router.push({path: "/myrestaurant"})
+  GoToAddRestaurant(): void {
+    this.$router.push({ path: "/myrestaurant" });
   }
 
-  GoToHome(): void{
-    this.$router.push({path: "/"})
+  GoToHome(): void {
+    this.$router.push({ path: "/" });
   }
 
-  GoToMyProfile(): void{
-    this.$router.push({path: "/myprofile"})
+  GoToMyProfile(): void {
+    this.$router.push({ path: "/myprofile" });
   }
-
-  readonly $appAuth!: FirebaseAuth;
 
   userLoggedIn(): boolean {
-  return this.$appAuth.currentUser?.uid !== undefined;
-}
+    return this.$appAuth.currentUser?.uid !== undefined;
+  }
 
-doLogout(): void {
-  this.$appAuth.signOut();
-  this.$router.back();    // Go backward in the "history stack"
+  doLogout(): void {
+    this.$appAuth.signOut();
+    this.$router.back(); // Go backward in the "history stack"
+  }
 }
-  
-}
-
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div >
+      <h1>Local Favorites</h1>
+      <div id="restCards" v-for="(z, pos) in restaurants" :key="pos">
+        <img :src="z.picture" alt="" style="width:300px;height:200px;" />
+        <div id="restInfo">
+        <h2>{{ z.name }}</h2>
+        <h3>Address: {{ z.address }}</h3>
+        <h3>Phone Number: {{ z.phone }}</h3>
+        <h3>Cuisine: {{ z.cuisine }}</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { FirebaseAuth } from "@firebase/auth-types";
+import {
+  FirebaseFirestore,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+} from "@firebase/firestore-types";
+@Component
+export default class Home extends Vue {
+  readonly $appDB!: FirebaseFirestore;
+  readonly $appAuth!: FirebaseAuth;
+  readonly $router!: any;
+  private restaurants: any[] = [];
+  mounted(): void {
+    console.log("Test");
+    this.$appDB
+      .collection("restaurants")
+      .orderBy("name")
+      .onSnapshot((qs: QuerySnapshot) => {
+        this.restaurants.splice(0);
+        qs.forEach((qds: QueryDocumentSnapshot) => {
+          if (qds.exists) {
+            const restData = qds.data();
+            this.restaurants.push({
+              name: restData.name,
+              cuisine: restData.cuisine,
+              address: restData.address,
+              phone: restData.phone,
+              picture: restData.picture,
+            });
+          }
+        });
+      });
+  }
+}
+</script>
+
+<style>
+#restCards {
+  margin: 1em;
+  display: inline-block;
+  flex-direction: column;
+ 
+}
+
+#restCards h3 {
+    text-align: left;
+}
+
+#restInfo {
+    
+    text-align: left;
+}
+
+img{
+    
+    vertical-align: middle;
+}
+</style>
